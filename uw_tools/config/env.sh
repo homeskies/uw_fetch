0672414f1a4159c82d8c6b2e01c8e4d1f4365ba5
Nick Walker <nswalker@cs.washington.edu>
2020-02-29 21:36:16 -0800

    Make keynames more specific
    Avoids problem of having all lab machines use the same username

--- uw_tools/config/env.sh
+++ uw_tools/config/env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 export ROBOT_NAME=astro
 export ROBOT_HOSTNAME=astro.cs.washington.edu
-export ROBOT_KEYNAME="${USER}_${ROBOT_NAME}_rsa"
+export ROBOT_KEYNAME="${USER}_`hostname`_${ROBOT_NAME}_rsa"
 export ROBOT_USERNAME=fetch
-export EXTERNAL_COMPUTER_HOSTNAME=astro-xavier.local
+export EXTERNAL_COMPUTER_HOSTNAME=astro-xavier
 export EXTERNAL_COMPUTER_USERNAME=robocup
 export REMOTE_WORKSPACE="${USER}_ws"
