265dd6e93847188936fb12de8cac23082a9b36ec
Nick Walker <nswalker@cs.washington.edu>
2020-07-18 20:27:52 -0700

    Add get_state method to Gripper
    Makes it easier to check whether a grasp succeeded

--- fetch_api/src/fetch_api/gripper.py
+++ fetch_api/src/fetch_api/gripper.py
@@ -3,6 +3,7 @@
 import actionlib
 import control_msgs.msg
 import rospy
+from sensor_msgs.msg import JointState
 
 CLOSED_POS = 0.0  # The position for a fully-closed gripper (meters).
 OPENED_POS = 0.10  # The position for a fully-open gripper (meters).
@@ -37,3 +38,20 @@ class Gripper(object):
         goal.command.position = CLOSED_POS
         goal.command.max_effort = max_effort
         self._client.send_goal_and_wait(goal, rospy.Duration(10))
+
+    def get_state(self, max_tries=20):
+        """
+        Queries the current joint angle of the gripper
+        :param max_tries: the maximum number of times to try to retrieve the joint states
+        :return: the gripper finger position in meters, or None if it couldn't be retrieved
+        """
+        i = 0
+        while i < max_tries:
+            joint_states = rospy.wait_for_message("/joint_states", JointState, timeout=rospy.Duration(1.0))
+            # In simulation, all joints are published in a single message. On the robot, gripper is
+            # separate.
+            if "l_gripper_finger_joint" in joint_states.name:
+                # We'll assume that both fingers are in same position
+                index = joint_states.name.index("l_gripper_finger_joint")
+                return joint_states.position[index]
+        return None
